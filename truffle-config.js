551ceae03c802d4d866634bd4eb44b4f104c3ba7
John Wong <jwwwwong@gmail.com>
2020-05-29 13:36:12 +0800

    Use 50 gwei for ropsten deployments

--- truffle-config.js
+++ truffle-config.js
@@ -15,7 +15,7 @@ module.exports = {
                 )
             },
             network_id: 3,
-            gasPrice: 10 * 1000000000
+            gasPrice: 50 * 1000000000
         },
         mainnet: {
             provider: function() {
