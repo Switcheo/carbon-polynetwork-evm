52667c56fbbd0e947b9c265c93d15d1762c4b149
John Wong <jwwwwong@gmail.com>
2020-07-15 13:56:11 +0800

    Update wallet factory migration

--- migrations/4_deploy_wallet_factory.js
+++ migrations/4_deploy_wallet_factory.js
@@ -1,5 +1,7 @@
 const WalletFactory = artifacts.require('WalletFactory')
 
 module.exports = function(deployer) {
-    deployer.deploy(WalletFactory)
+    const targetChainId = 173
+    const lockProxyAddress = '0x8eb220f0d4dcd610a5208753366557270e899bf2'
+    deployer.deploy(WalletFactory, targetChainId, lockProxyAddress)
 }
