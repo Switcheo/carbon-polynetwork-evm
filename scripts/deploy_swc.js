0e288804ab5ad1a1133078246b2b0ac3682746cf
holyxiaoxin <jr.loves.maths@gmail.com>
2022-06-09 17:08:49 +0800

    feat: add SWCoin

---
+++ scripts/deploy_swc.js
@@ -0,0 +1,32 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require('hardhat')
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile')
+
+  // We get the contract to deploy
+  const SWCoin = await hre.ethers.getContractFactory('SWCoin')
+  const swCoin = await SWCoin.deploy('SWC5')
+
+  await swCoin.deployed()
+
+  console.log('SWCoin deployed to:', swCoin.address)
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
