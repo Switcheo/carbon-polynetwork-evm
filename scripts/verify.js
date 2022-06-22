2465aaac66a2e0721abc135a6237ff68e4cf9c40
holyxiaoxin <jr.loves.maths@gmail.com>
2022-06-22 18:52:21 +0800

    feat: update verify address

--- scripts/verify.js
+++ scripts/verify.js
@@ -8,8 +8,8 @@ async function main() {
   // update addresses!!!
   const swthAddress = '0x32e125258b7db0a0dffde5bd03b2b859253538ab'
   const ccmProxyAddress = '0x6564BAd8ab4967EA48bef7bF224801E92DBb6Be9'
-  const lockProxyAddress = '0xFE5C9832b62bFfFFfD1B091de457254dab344C04'
-  const bridgeEntranceAddress = '0x1fFf3678De3b2e250eccB214078902DcEe08748B'
+  const lockProxyAddress = '0xb5D4f343412dC8efb6ff599d790074D0f1e8D430'
+  const bridgeEntranceAddress = '0x93fD29fF3B662c9e5e15681bb3B139D6CE2Ca9C5'
 
   const LockProxy = await hre.ethers.getContractFactory('LockProxy')
   const lockProxy = await LockProxy.attach(lockProxyAddress)
