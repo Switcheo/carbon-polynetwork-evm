fee6696b7f7c1b97b3a9fc9119c2af0febb8f268
Steven Khong <khongwh@gmail.com>
2023-02-01 18:09:37 +0800

    Implement quote proxy reader for oracle

--- contracts/QuoteProxyReader.sol
+++ contracts/QuoteProxyReader.sol
@@ -3,25 +3,25 @@
 pragma solidity ^0.8.0;
 
 interface IQuoterV2 {
-    struct QuoteOutput {
-        uint256 usedAmount;
-        uint256 returnedAmount;
-        uint160 afterSqrtP;
-        uint32 initializedTicksCrossed;
-        uint256 gasEstimate;
-    }
+	struct QuoteOutput {
+		uint256 usedAmount;
+		uint256 returnedAmount;
+		uint160 afterSqrtP;
+		uint32 initializedTicksCrossed;
+		uint256 gasEstimate;
+	}
 
-    function quoteExactInputSingle(QuoteExactInputSingleParams memory params)
-        external
-        returns (QuoteOutput memory);
+	function quoteExactInputSingle(QuoteExactInputSingleParams memory params)
+		external
+		returns (QuoteOutput memory);
 
-    struct QuoteExactInputSingleParams {
-        address tokenIn;
-        address tokenOut;
-        uint256 amountIn;
-        uint24 feeUnits;
-        uint160 limitSqrtP;
-    }
+  struct QuoteExactInputSingleParams {
+    address tokenIn;
+    address tokenOut;
+    uint256 amountIn;
+    uint24 feeUnits;
+    uint160 limitSqrtP;
+	}
 }
 
 contract QuoteProxyReader {
@@ -32,21 +32,16 @@ contract QuoteProxyReader {
         uint256 amountIn,
         uint24 feeUnits,
         uint160 limitSqrtP
-    ) public view returns (uint256) {
-        IQuoterV2.QuoteExactInputSingleParams memory params = IQuoterV2
-            .QuoteExactInputSingleParams({
-                tokenIn: tokenIn,
-                tokenOut: tokenOut,
-                amountIn: amountIn,
-                feeUnits: feeUnits,
-                limitSqrtP: limitSqrtP
-            });
-        bytes memory payload = abi.encodeWithSignature("quoteExactInputSingle((address,address,uint256,uint24,uint160))", params);
-        (bool success, bytes memory result) = quoteContract.staticcall(payload);
-				require(success, "quoteExactInputSingle return not successful");
+    ) public returns (uint256) {
+        IQuoterV2.QuoteExactInputSingleParams memory params = IQuoterV2.QuoteExactInputSingleParams({
+						tokenIn: tokenIn,
+						tokenOut: tokenOut,
+						amountIn: amountIn,
+						feeUnits: feeUnits,
+						limitSqrtP: limitSqrtP
+				});
 
-				IQuoterV2.QuoteOutput memory output = abi.decode(result, (IQuoterV2.QuoteOutput));
-
+        IQuoterV2.QuoteOutput memory output = IQuoterV2(quoteContract).quoteExactInputSingle(params);
 				require(output.usedAmount == amountIn, "usedAmount not equals to amountIn");
 
 				return output.returnedAmount;
